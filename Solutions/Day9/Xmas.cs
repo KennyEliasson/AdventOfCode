04686884c5cab453c27492fa97456c24662dda28
Kenny Eliasson <kennyeliasson@phoniro.com>
2020-12-09 09:31:21 +0100

    Solve day9

---
+++ Solutions/Day9/Xmas.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Solutions.Day9
+{
+    public class Xmas
+    {
+        public long FindFirstNumberThatNotMatchesPremable(List<long> input, int preambleLength)
+        {
+            return FindFirstNumberThatNotMatchesPremable(input, 0, preambleLength);
+        }
+
+        private long FindFirstNumberThatNotMatchesPremable(List<long> input, int startIndex, int preambleLength)
+        {
+            while (true)
+            {
+                var range = startIndex..preambleLength;
+                var slice = input.GetRange(range.Start.Value, range.End.Value);
+                var possibilities = CalculatePossibilities(slice);
+                
+                var numberToFind = input[range.Start.Value + range.End.Value];
+
+                if (possibilities.Contains(numberToFind))
+                {
+                    startIndex++;
+                }
+                else
+                {
+                    return numberToFind;
+                }
+            }
+        }
+
+        private IEnumerable<long> CalculatePossibilities(List<long> start)
+        {
+            foreach (var leftTerm in start)
+            {
+                foreach (var rightTerm in start)
+                {
+                    if(leftTerm == rightTerm)
+                        continue;
+
+                    yield return leftTerm + rightTerm;
+                }
+            }
+        }
+
+        public long FindRangeThatMatches(List<long> numbers, long numberToMatchAgainst)
+        {
+            var (startIndex, endIndex) = CalculatePossibilitesToFindMatchingSequence(numbers, numberToMatchAgainst);
+            var range = numbers.GetRange(startIndex, endIndex-startIndex);
+            var rangeMin = range.Min();
+            var rangeMax = range.Max();
+
+            return rangeMin + rangeMax;
+        }
+
+        private (int startIndex, int endIndex) CalculatePossibilitesToFindMatchingSequence(IReadOnlyList<long> numbers, long numberToMatchAgainst, int startIndex = 0)
+        {
+            while (true)
+            {
+                long match = 0;
+                for (var i = startIndex; i < numbers.Count; i++)
+                {
+                    match += numbers[i];
+
+                    if (numberToMatchAgainst == match)
+                    {
+                        return (startIndex, i);
+                    }
+
+                    if (match > numberToMatchAgainst)
+                    {
+                        startIndex++;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Verifications/Day9/Day9Tests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Solutions.Day9;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Verifications.Day9
+{
+    public class Day9Tests
+    {
+        private readonly ITestOutputHelper _outputHelper;
+
+        public Day9Tests(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+        
+        [Fact]
+        public void Task1_Example()
+        {
+            var numbers = @"35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576";
+            var xmas = new Xmas();
+            var result = xmas.FindFirstNumberThatNotMatchesPremable(numbers.Split(Environment.NewLine).Select(long.Parse).ToList(), 5);
+            
+            Assert.Equal(127, result);
+}
+
+        [Fact]
+        public void Task1()
+        {
+            var numbers = File.ReadAllLines("Day9/input.txt").Select(long.Parse).ToList();
+           var xmas = new Xmas();
+           var result = xmas.FindFirstNumberThatNotMatchesPremable(numbers, 25);
+           
+           _outputHelper.WriteLine($"The result is {result.ToString()}");
+        }
+        
+        [Fact]
+        public void Task2_Example()
+        {
+            var numbers = @"35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576";
+            var xmas = new Xmas();
+            var result = xmas.FindRangeThatMatches(numbers.Split(Environment.NewLine).Select(long.Parse).ToList(), 127);
+           
+            _outputHelper.WriteLine($"The result is {result.ToString()}");
+        }
+        
+        [Fact]
+        public void Task2()
+        {
+            var numbers = File.ReadAllLines("Day9/input.txt").Select(long.Parse).ToList();
+            var xmas = new Xmas();
+            var result = xmas.FindRangeThatMatches(numbers, 25918798);
+           
+            _outputHelper.WriteLine($"The result is {result.ToString()}");
+        }
+    } 
+}
