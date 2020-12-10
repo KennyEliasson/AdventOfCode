4491404881baac9ccfab41c14aef96e97f1891a7
Kenny Eliasson <kennyeliasson@phoniro.com>
2020-12-10 09:59:34 +0100

    Solved day10

---
+++ Solutions/Day10/ChargingAdapters.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection.Emit;
+
+namespace Solutions.Day10
+{
+    public class ChargingAdapters
+    {
+        public List<int> CalculateAdapterDifferences(List<int> adapters)
+        {
+            adapters = adapters.OrderBy(x => x).ToList();
+
+            var differences = new List<int>();
+            var currentAdapter = 0;
+            for (int i = 0; i < adapters.Count;i++)
+            {
+                var diff = adapters[i] - currentAdapter;
+                if(diff <= 3)
+                {
+                    differences.Add(diff);
+                    currentAdapter = adapters[i];
+                }
+            }
+            
+            // Last for the device
+            differences.Add(3);
+
+            return differences;
+        } 
+        
+        private readonly Dictionary<int, long> _alreadyCalculated = new Dictionary<int, long>();
+        
+        public long CalculatePossibilities(List<int> adapters)
+        {
+            adapters.Add(0);
+            adapters.Add(adapters.Max() + 3);
+            adapters = adapters.OrderBy(x => x).ToList();
+            _alreadyCalculated[adapters.Count - 1] = 0;
+            _alreadyCalculated[adapters.Count - 2] = 1;
+
+
+            var val = CalculatePossibilitiesFrom(0, adapters);
+            return val;
+        }
+
+        private long CalculatePossibilitiesFrom(int startIndex, List<int> adapters)
+        {
+
+            if (_alreadyCalculated.ContainsKey(startIndex))
+                return _alreadyCalculated[startIndex];
+            
+            long currentVal = 0;
+
+            for (int i = 1; i <= 3; i++)
+            {
+                var currentIndex = startIndex + i;
+                if (currentIndex >= adapters.Count)
+                {
+                    break;
+                }
+
+                var diff = adapters[currentIndex] - adapters[startIndex];
+                if (diff <= 3)
+                {
+                    currentVal += CalculatePossibilitiesFrom(currentIndex, adapters);
+                }
+            }
+
+            _alreadyCalculated[startIndex] = currentVal;
+
+            return currentVal;
+        }
+    }
+}
---
+++ Verifications/Day10/Day10Tests.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Solutions.Day10;
+using Solutions.Day9;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Verifications.Day10
+{
+    public class Day10Tests
+    {
+        private readonly ITestOutputHelper _outputHelper;
+
+        public Day10Tests(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+        
+        [Fact]
+        public void Task1_Example()
+        {
+            var adapters = new List<int> {
+                16,
+                10,
+                15,
+                5,
+                1,
+                11,
+                7,
+                19,
+                6,
+                12,
+                4
+            };
+            
+            var chargingAdapters = new ChargingAdapters();
+            var differences = chargingAdapters.CalculateAdapterDifferences(adapters);
+
+            Assert.Equal(7, differences.Count(x => x == 1));
+            Assert.Equal(5, differences.Count(x => x == 3));
+        }
+        
+        [Fact]
+        public void Task1_Example_2()
+        {
+            var adapters = new List<int> {
+                28,
+                33,
+                18,
+                42,
+                31,
+                14,
+                46,
+                20,
+                48,
+                47,
+                24,
+                23,
+                49,
+                45,
+                19,
+                38,
+                39,
+                11,
+                1,
+                32,
+                25,
+                35,
+                8,
+                17,
+                7,
+                9,
+                4,
+                2,
+                34,
+                10,
+                3
+            };
+            
+            var chargingAdapters = new ChargingAdapters();
+            var differences = chargingAdapters.CalculateAdapterDifferences(adapters);
+
+            Assert.Equal(22, differences.Count(x => x == 1));
+            Assert.Equal(10, differences.Count(x => x == 3));
+        }   
+
+        [Fact]
+        public void Task1()
+        {
+            var adapters = File.ReadAllLines("Day10/input.txt").Select(int.Parse).ToList();
+            var chargingAdapters = new ChargingAdapters();
+            var differences = chargingAdapters.CalculateAdapterDifferences(adapters);
+
+            var oneDiff = differences.Count(x => x == 1);
+            var threeDiff = differences.Count(x => x == 3);
+
+            var result = oneDiff * threeDiff;
+            
+            _outputHelper.WriteLine($"The result is {result.ToString()}");
+        }
+        
+        [Fact]
+        public void Task2_Example1()
+        {
+            var adapters = new List<int> {
+                16,
+                10,
+                15,
+                5,
+                1,
+                11,
+                7,
+                19,
+                6,
+                12,
+                4
+            };
+            
+            var chargingAdapters = new ChargingAdapters();
+            var possibilities = chargingAdapters.CalculatePossibilities(adapters);
+            
+            Assert.Equal(8, possibilities);
+        }
+        
+        [Fact]
+        public void Task2_Example_2()
+        {
+            var adapters = new List<int> {
+                28,
+                33,
+                18,
+                42,
+                31,
+                14,
+                46,
+                20,
+                48,
+                47,
+                24,
+                23,
+                49,
+                45,
+                19,
+                38,
+                39,
+                11,
+                1,
+                32,
+                25,
+                35,
+                8,
+                17,
+                7,
+                9,
+                4,
+                2,
+                34,
+                10,
+                3
+            };
+            
+            var chargingAdapters = new ChargingAdapters();
+            var possibilities = chargingAdapters.CalculatePossibilities(adapters);
+
+            Assert.Equal(19208 , possibilities);
+        }  
+        
+        [Fact]
+        public void Task2()
+        {
+            var adapters = File.ReadAllLines("Day10/input.txt").Select(int.Parse).ToList();
+            var chargingAdapters = new ChargingAdapters();
+            var possibilities = chargingAdapters.CalculatePossibilities(adapters);
+
+           
+            _outputHelper.WriteLine($"The result is {possibilities.ToString()}");
+        }
+        
+    } 
+}
